a971193f3271751a58bdf1be7c516d3466b1a29b
콘텐츠검색팀 이재환 <victory56@sk.com>
2023-08-22 11:31:22 +0900

    input file

---
+++ src/input/__init__.py
@@ -0,0 +1,3 @@
+from src.input import SimcseInput
+from src.input import TokenizerInput
+from src.input import StsInput
---
+++ src/input/input.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from dataclasses import dataclass
+from typing import List, Any, Union, Dict
+
+
+@dataclass
+class TokenizerInput:
+    add_special_tokens:bool = True
+    max_length:int = None
+    pad_to_max_length:bool = False
+    return_attention_mask:bool = True
+    return_token_type_ids:bool = False
+
+
+@dataclass
+class SimcseInput:
+    sentence_a: str = None
+    sentence_b: str = None
+    sentence_c: str = None
+    a_input_ids: List[int] = None
+    a_attention_mask: List[int] = None
+    b_input_ids: List[int] = None
+    b_attention_mask: List[int] = None
+    c_input_ids: List[int] = None
+    c_attention_mask: List[int] = None
+
+
+@dataclass
+class StsInput:
+    sentence_a: str = None
+    sentence_b: str = None
+    sentence_c: str = None
+    a_input_ids: List[int] = None
+    a_attention_mask: List[int] = None
+    b_input_ids: List[int] = None
+    b_attention_mask: List[int] = None
+    label: float = None
+
+
+    

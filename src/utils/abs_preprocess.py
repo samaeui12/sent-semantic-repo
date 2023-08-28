c6ea47feab4002bcfb4d056e3b23759573971c5c
콘텐츠검색팀 이재환 <victory56@sk.com>
2023-08-28 15:34:50 +0900

    add @classmethod tokenizing for common AbsPreprocessor

--- src/utils/abs_preprocess.py
+++ src/utils/abs_preprocess.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pandas as pd
+from transformers.tokenization_utils import PreTrainedTokenizer
+from input import TokenizerInput
+import dataclasses
 
 from abc import *
 class AbsPreprocessor(metaclass=ABCMeta):
+    @classmethod         
+    def tokenizing(cls, input, tokenizer:PreTrainedTokenizer, tokenizer_input:TokenizerInput=None):
+        if tokenizer_input is None:
+            return tokenizer.encode_plus(input)
+        else:
+            kwargs = dataclasses.asdict(tokenizer_input)
+            return tokenizer.encode_plus(input, **kwargs)
+
     @abstractmethod
     def preprocess(self) -> pd.DataFrame:
         """preprocessing raw data"""

29694202a13c1b43b9bb34a0f4b95fc0d4097702
콘텐츠검색팀 이재환 <victory56@sk.com>
2023-08-28 18:24:58 +0900

    Unsup sup

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,4 +1,4 @@
 from src.dataset.abs_dataloader import AbstractDataloader
-from src.dataset.nli_dataset import NliDataSet
-from src.dataset.simcse_dataloader import SimcseDataloader
+from src.dataset.simcse_dataset import NliDataSet
+from src.dataset.simcse_dataset import Unsup_simcse, Sup_simcse
 from src.dataset.sts_dataset import StsDataset

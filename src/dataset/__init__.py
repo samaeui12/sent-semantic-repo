975f5696fa7e528ea67212035817038775dacdb7
콘텐츠검색팀 이재환 <victory56@sk.com>
2023-08-28 18:26:10 +0900

    dataset mapper added

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -2,3 +2,10 @@ from src.dataset.abs_dataloader import AbstractDataloader
 from src.dataset.simcse_dataset import NliDataSet
 from src.dataset.simcse_dataset import Unsup_simcse, Sup_simcse
 from src.dataset.sts_dataset import StsDataset
+
+ 
+DATASET_MAPPING_DICT = {
+    'Sup_simcse': Sup_simcse,
+    'Unsup_simcse': Unsup_simcse,
+    'StsDataset': StsDataset
+}

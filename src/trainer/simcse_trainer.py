0a88e8b99ecf56fdedfe3bbf4eca3adae7bf5386
콘텐츠검색팀 이재환 <victory56@sk.com>
2023-08-29 13:24:03 +0900

    add printg

--- src/trainer/simcse_trainer.py
+++ src/trainer/simcse_trainer.py
@@ -157,6 +157,7 @@ class SimcseTrainer(AbstractTrainer):
         a_embedding = self.model(batch['a_input_ids'], batch['a_attention_mask'])
         b_embedding = self.model(batch['b_input_ids'], batch['b_attention_mask'])
         c_embedding = self.model(batch['c_input_ids'], batch['c_attention_mask'])
+        print(a_embedding.size())
         a_norm = a_embedding / a_embedding.norm(dim=1)[:, None]
         b_norm = b_embedding / b_embedding.norm(dim=1)[:, None]
         c_norm = c_embedding / c_embedding.norm(dim=1)[:, None]

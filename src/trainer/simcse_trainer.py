e958e85e817c9bdc422ff03924fcfa01d041df69
검색추천팀 윤기태 <skt.1113525@sk.com>
2023-09-05 13:19:52 +0900

    test

--- src/trainer/simcse_trainer.py
+++ src/trainer/simcse_trainer.py
@@ -110,8 +110,8 @@ class SimcseTrainer(AbstractTrainer):
             self.tokenizer = tokenizer
 
         """ nn.distributed setting Not implemented yet """
-        if self.args.n_gpu > 1 and not isinstance(self.model, torch.nn.DataParallel):
-            self.model = torch.nn.DataParallel(self.model, device_ids=[0,1])
+        # if self.args.n_gpu > 1 and not isinstance(self.model, torch.nn.DataParallel):
+        #     self.model = torch.nn.DataParallel(self.model, device_ids=[0,1])
 
         self.model.to(self.args.device)
 

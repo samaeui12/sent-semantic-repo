fb644544525bdc7e7f2026f834e2cbc1990d86d9
콘텐츠검색팀 이재환 <victory56@sk.com>
2023-09-06 18:52:50 +0900

    no change

--- src/config/nli_config.py
+++ src/config/nli_config.py
@@ -82,7 +82,7 @@ def nli_parser_model_args():
     parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
     parser.add_argument(
         "--num_train_epochs",
-        default=10,
+        default=30,
         type=float,
         help="Total number of training epochs to perform.",
     )

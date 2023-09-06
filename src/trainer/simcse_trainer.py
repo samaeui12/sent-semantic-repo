6e0896500d8c088ae2c9e9c5e67e6de967eb801e
콘텐츠검색팀 이재환 <victory56@sk.com>
2023-09-06 17:37:45 +0900

    no change

--- src/trainer/simcse_trainer.py
+++ src/trainer/simcse_trainer.py
@@ -149,7 +149,7 @@ class SimcseTrainer(AbstractTrainer):
         )   
         loss_fct = Loss_MAPPING_DICT[self.args.loss]
         self.loss_fct = loss_fct(margin=self.args.margin, temperature=self.args.temperature)
-        self.metrics = self.initialize_metrics(metrics=['val_loss'])
+        self.metrics = self.initialize_metrics(metrics=[self.args.metric])
         
     def cal_loss(self, batch):
 

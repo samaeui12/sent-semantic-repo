88582bfb2f280cc08544f29fd9ae8cd23940ea0a
jaehwan <victory56@sk.com>
2023-11-28 14:13:40 +0900

    inference dataset add

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,4 +1,5 @@
 from .simcse_dataset import Unsup_simcse, Sup_simcse
+from .inference_dataset import InferenceDataset
 from .sts_dataset import StsDataset
 from .inference_dataset import InferenceDataset
 
@@ -7,5 +8,5 @@ DATASET_MAPPING_DICT = {
     'Sup_simcse': Sup_simcse,
     'Unsup_simcse': Unsup_simcse,
     'StsDataset': StsDataset,
-    'InferenceDataset: InferenceDataset 
+    'InferenceDataset': InferenceDataset 
 }
